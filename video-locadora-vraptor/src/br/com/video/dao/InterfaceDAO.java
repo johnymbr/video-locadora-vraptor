168567a8d9362d7ae72c328da35d1c5274c7c7ab
johny <johny@johny-Ubuntu>
2011-07-18 22:24:01 -0300

    Criacao de pacotes para BO's e DAO's e tambem interfaces e as classes UserBO e UserDAO.

---
+++ video-locadora-vraptor/src/br/com/video/dao/InterfaceDAO.java
@@ -0,0 +1,11 @@
+package br.com.video.dao;
+
+/**
+ * Interface para as classes DAO.
+ * @author johny
+ *
+ * @param <T> Tipo do objeto que sera utilizado na implementacao.
+ */
+public interface InterfaceDAO<T> {
+
+}

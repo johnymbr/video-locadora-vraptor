5daea922a0a719ce4382e67651b475a96144f5fc
johny <johny@johny-Ubuntu>
2011-07-27 18:28:35 -0300

    Criação e anotação da entity user.

--- video-locadora-vraptor/src/br/com/video/entity/User.java
+++ video-locadora-vraptor/src/br/com/video/entity/User.java
@@ -1,16 +1,98 @@
 package br.com.video.entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 /**
  * Classe que ira representar um usuário do sistema.
  * @author johny
- *
  */
-
 @Entity
 @Table(name="tb_user")
 public class User {
+
+	@Id
+	@GeneratedValue
+	@Column(name="user_id")
+	private Integer id;
 	
+	@Column(name="user_name", length=150, nullable=false)
+	private String name;
+	
+	@Column(name="user_username", length=150, nullable=false)
+	private String username;
+	
+	@Column(name="user_hash_password", nullable=false)
+	private String hashPassword;
+	
+	@Column(name="user_salt", nullable=false)
+	private String salt;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getHashPassword() {
+		return hashPassword;
+	}
+
+	public void setHashPassword(String hashPassword) {
+		this.hashPassword = hashPassword;
+	}
+
+	public String getSalt() {
+		return salt;
+	}
+
+	public void setSalt(String salt) {
+		this.salt = salt;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
 }
